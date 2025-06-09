1c88a2f05c1ee07d70026f13dbedd9d951564c71
Imed <hamami.imed@gmail.com>
2025-06-09 13:18:53 +0200

    refactor and add logging

--- src/main/java/com/example/kata/service/impl/TransformServiceImpl.java
+++ src/main/java/com/example/kata/service/impl/TransformServiceImpl.java
@@ -1,22 +1,34 @@
 package com.example.kata.service.impl;
 
 import com.example.kata.service.TransformService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class TransformServiceImpl implements TransformService {
+
     @Override
     public String transform(int number) {
+        log.info("Transform called with input: {}", number);
         if (number < 0 || number > 100) {
+            log.warn("Invalid input: {}", number);
             throw new IllegalArgumentException("Le nombre doit être entre 0 et 100.");
         }
         StringBuilder result = new StringBuilder();
+
         if (number % 3 == 0) result.append("FOO");
         if (number % 5 == 0) result.append("BAR");
+
         char[] chars = String.valueOf(number).toCharArray();
         for (char c : chars) {
             if (c == '3') result.append("FOO");
             if (c == '5') result.append("BAR");
             if (c == '7') result.append("QUIX");
         }
-        return result.isEmpty() ? String.valueOf(number) : result.toString();
+
+        String output = !result.isEmpty() ? result.toString() : String.valueOf(number);
+        log.debug("Transformed result: {}", output);
+        return output;
     }
 }
