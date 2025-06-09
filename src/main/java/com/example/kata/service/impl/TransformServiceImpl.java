b4d35059994c9302dca6c352063effc8526e894e
Imed <hamami.imed@gmail.com>
2025-06-09 12:54:16 +0200

    feat: return FOO when number is divisible by 3

--- src/main/java/com/example/kata/service/impl/TransformServiceImpl.java
+++ src/main/java/com/example/kata/service/impl/TransformServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.kata.service.TransformService;
 public class TransformServiceImpl implements TransformService {
     @Override
     public String transform(int number) {
-        return String.valueOf(number);
+        StringBuilder result = new StringBuilder();
+        if (number % 3 == 0) result.append("FOO");
+        return result.isEmpty() ? String.valueOf(number) : result.toString();
     }
 }
