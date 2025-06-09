ccbc7a1ce9f5cbcf2a5eb2a4c42ff5a035db3f9e
Imed <hamami.imed@gmail.com>
2025-06-09 12:40:16 +0200

    feat: add empty implementation for TransformService

---
+++ src/main/java/com/example/kata/service/impl/TransformServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.kata.service.impl;
+
+import com.example.kata.service.TransformService;
+
+public class TransformServiceImpl implements TransformService {
+    @Override
+    public String transform(int number) {
+        return null; // implémentation vide pour TDD
+    }
+}
