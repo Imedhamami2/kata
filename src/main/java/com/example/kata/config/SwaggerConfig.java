2d89a73ab1e838665765642ab519d09b2ed3e092
Imed <hamami.imed@gmail.com>
2025-06-09 14:35:12 +0200

    chore: add SwaggerConfig to customize OpenAPI documentation

---
+++ src/main/java/com/example/kata/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.example.kata.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Kata FooBarQuix API")
+                        .version("1.0")
+                        .description("API REST pour transformer des nombres selon les règles Foo/Bar/Quix"))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local dev")
+                ));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
