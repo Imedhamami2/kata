cab1657691f5bf0032b0ca153f8c1ef336543398
Imed <hamami.imed@gmail.com>
2025-06-09 13:06:47 +0200

    test: add tests for transform() method to check validate input

--- src/test/java/com/example/kata/service/impl/TransformServiceImplTest.java
+++ src/test/java/com/example/kata/service/impl/TransformServiceImplTest.java
@@ -1,11 +1,13 @@
 package com.example.kata.service.impl;
 
 import com.example.kata.service.TransformService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class TransformServiceImplTest {
@@ -15,5 +17,14 @@ public class TransformServiceImplTest {
     @MethodSource("com.example.kata.TestDataProvider#provideFooBarQuixData")
     public void testTransform(int input, String expected) {
         assertEquals(expected, service.transform(input));
+    }
+    @Test
+    public void testTransformThrowsExceptionForNegative() {
+        assertThrows(IllegalArgumentException.class, () -> service.transform(-1));
+    }
+
+    @Test
+    public void testTransformThrowsExceptionAboveRange() {
+        assertThrows(IllegalArgumentException.class, () -> service.transform(101));
     }
 }
