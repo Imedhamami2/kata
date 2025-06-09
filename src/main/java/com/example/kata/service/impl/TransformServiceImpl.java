92e15cb2158d61a1aa0bb22e5ad754392f2e5de8
Imed <hamami.imed@gmail.com>
2025-06-09 12:59:04 +0200

    feat: append FOO for each digit 3 in the number

--- src/main/java/com/example/kata/service/impl/TransformServiceImpl.java
+++ src/main/java/com/example/kata/service/impl/TransformServiceImpl.java
@@ -8,6 +8,10 @@ public class TransformServiceImpl implements TransformService {
         StringBuilder result = new StringBuilder();
         if (number % 3 == 0) result.append("FOO");
         if (number % 5 == 0) result.append("BAR");
+        char[] chars = String.valueOf(number).toCharArray();
+        for (char c : chars) {
+            if (c == '3') result.append("FOO");
+        }
         return result.isEmpty() ? String.valueOf(number) : result.toString();
     }
 }
